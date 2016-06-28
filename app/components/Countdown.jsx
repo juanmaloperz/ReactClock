cca82ef0af40071a10ebb4e1823acf49186c59b0
Juan Lopez <juanmaloperz@gmail.com>
2016-06-28 15:02:37 -0500

    Add countdown Form

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,22 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+   getInitialState:function () {
+     return {count:0};
+   },
+  handlesetCountdown:function(seconds){
+    this.setState({
+      count: seconds
+    });
+  },
 render: function(){
+  var {count} = this.state;
   return(
     <div>
-       <Clock totalSeconds={125}/> 
+       <Clock totalSeconds={count}/>
+       <CountdownForm onSetCountdown={this.handlesetCountdown}/>
     </div>
   )
 
