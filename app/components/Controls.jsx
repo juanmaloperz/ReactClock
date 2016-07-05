d7c20f8f125cf033d4e3f9064e2663a1419fdca1
Juan Lopez <juanmaloperz@gmail.com>
2016-07-05 15:13:30 -0500

    add status

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -18,11 +18,10 @@ var Controls = React.createClass({
    var renderStartStopButton = () =>{
      if (countdownStatus === 'started'){
        return <button className="button secondary" onClick={this.onStatusChange('paused')}> Pause </button>
-     } else if ( countdownStatus === 'paused'){
+     } else {
           return <button className="button primary" onClick={this.onStatusChange('started')}> Start </button>
      }
    };
-
    return (
      <div className="controls">
         {renderStartStopButton()}
--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,13 +1,60 @@
 var React = require('react');
+var Controls = require('Controls');
+var Clock = require('Clock');
 
 var Timer = React.createClass({
+  getInitialState: function (){
+    return {  count: 0,
+      countupStatus: 'stopped'
+    };
+  },
+
+  componentDidUpdate: function(prevProps, prevState){
+       if(this.state.countupStatus != prevState.countupStatus){
+         switch (this.state.countupStatus) {
+           case 'started':
+             this.starTime();
+             break;
+             case 'stopped':
+             this.setState({count:0});
+             case('paused'):
+             clearInterval(this.timer)
+             this.timer = undefined;
+             break;
+         }
+       }
+  },
+  componentWillUnmount: function(){
+    clearInterval(this.timer)
+  },
+
+  starTime: function(){
+      this.timer = setInterval(()=>{
+    var newCount = this.state.count+1;
+    this.setState({
+      count: newCount
+    });
+  },1000);
+},
+   handleStatusChange: function(newStatus){
+this.setState({countupStatus :newStatus});
+   },
+
+/*  handlesetCountup: function(seconds){
+    this.setState({
+      count:seconds
+    })
+  },  */
 render: function(){
+  var {count , countupStatus} = this.state;
   return(
     <div>
-      <p> Timer.jsx </p>
+      <h1 className="page-tittle"> Timer APP </h1>
+       <Clock totalSeconds={count}/>
+       <Controls countupStatus={countupStatus} onStatusChange={this.handleStatusChange}/>
     </div>
-  );
-}
+  )
+},
 });
 
 module.exports = Timer;
