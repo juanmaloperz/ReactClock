89df60d97735451a6678f82fe1b84fc626331973
Juan Lopez <juanmaloperz@gmail.com>
2016-10-10 15:34:14 -0500

    adding ptoducts

--- app/components/Productos.jsx
+++ app/components/Productos.jsx
@@ -4,7 +4,8 @@ var Productos = React.createClass({
 render: function(){
 return(
   <div>
-
+    <h1> Bloques </h1>
+   <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/v1476131361/BLOQUESARZUL1_vq1s0t.jpg'}/>
   </div>
 )
 }
--- app/components/Socios.jsx
+++ app/components/Socios.jsx
@@ -7,9 +7,12 @@ return(
        <div className="integrantes">
        <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/c_scale,r_1,w_2143/v1476113267/LOGO_ARZUL_pn8ecv.jpg'}/>
        </div>
+       <div>
+        <h1> Nuestros Socios </h1>
+       </div>
        <div className="logos">
           <a href="http://zuligres.com"> <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/c_scale,h_134/v1476127910/zuligres_mbhujr.jpg'} className="lovely" /> </a>
-          <a href="http://tejarsantamaria.com"> <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/v1476129072/santamaria_gdlp5q.jpg'} className="lovely" /> </a>
+          <a href="http://tejarsantamaria.com"> <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/v1476129072/santamaria_gdlp5q.jpg'} className="lovely"  qº/> </a>
       </div>
   </div>
 )
