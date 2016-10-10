41fb7ae557073bbc24091bc1eae81319e0ae2485
Juan Lopez <juanmaloperz@gmail.com>
2016-10-10 17:37:39 -0500

    adding products

--- app/components/Productos.jsx
+++ app/components/Productos.jsx
@@ -3,9 +3,12 @@ var React = require('react');
 var Productos = React.createClass({
 render: function(){
 return(
-  <div>
-    <h1> Bloques </h1>
+  <div className="portada">
+    <div className="integrantes">
+    <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/c_scale,r_1,w_2143/v1476113267/LOGO_ARZUL_pn8ecv.jpg'}/>
+    </div>
    <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/v1476131361/BLOQUESARZUL1_vq1s0t.jpg'}/>
+   <img src={'http://res.cloudinary.com/dkr9ndsvc/image/upload/v1476138823/PRODUCTOS_ARZUL2_rqtoez.jpg'}/>
   </div>
 )
 }
